3b905cd70fec07d6813914b7fbc7eac42b5097a3
surh <surh@users.noreply.github.com>
2017-09-25 01:03:12 -0700

    comparing combinatorix and results from validation

--- inst/scripts/binvalidation_1.sizes.r
+++ inst/scripts/binvalidation_1.sizes.r
@@ -121,7 +121,7 @@ analyze_measurement <- function(variable, dat, trans = "log2",
 }
 ##################################################################
 
-setwd("~/rhizogenomics/experiments/2017/today/")
+#setwd("~/rhizogenomics/experiments/2017/today/")
 
 ################ ANALYZE ################
 # Size in pixels
@@ -168,5 +168,36 @@ res <- analyze_measurement(variable = "Normalized.hull.area",
 AIC(res$m1,res$m2)
 BIC(res$m1,res$m2)
 
+# Process validation coefs
+validation_coefs <- summary(res$m2)$coef
+validation_coefs <- data.frame(validation_coefs)
+validation_coefs <- validation_coefs[ grep("^Treatment", row.names(validation_coefs)), ]
+colnames(validation_coefs) <- c("Validation.Effect", "Validation.SE",
+                                "Validation.df","Validation.t","Validation.p")
+validation_coefs$Strain <- row.names(validation_coefs)
+validation_coefs$Strain <- gsub(pattern = "^Treatment", replacement = "", x = validation_coefs$Strain)
+validation_coefs
 
+
+#######
+data(comb.phenotypes)
+comb.phenotypes <- subset(comb.phenotypes,Phenotype == "hull.area")
+row.names(comb.phenotypes) <- sub(pattern = "^X",replacement = "",x = as.character(comb.phenotypes$Strain))
+head(comb.phenotypes)
+
+validation_coefs$Comb.Estimate <- comb.phenotypes[ validation_coefs$Strain, "Coefficient"]
+head(validation_coefs)
+
+p1 <- ggplot(validation_coefs, aes(x = Comb.Estimate, y = Validation.Effect)) +
+  geom_point(aes(color = Validation.p < 0.01), size = 3) +
+  geom_text(aes(label = Strain), vjust = 1.5) +
+  theme(panel.background = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_text(face = "bold", color = "black"),
+        axis.text = element_text(color = "black"),
+        panel.border = element_rect(color = "black", fill = NA, size= 2))
+p1
+ggsave("comparison_hull_area.svg", p1, width = 6, height = 4)
+
+subset(comb.phenotypes, p.value < 0.05)
 
